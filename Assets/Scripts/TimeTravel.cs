71a0a2bccfcc95c5d71fa0ba81ae505fd2a1d7f0
AndyMcGamer <andymcgamer@gmail.com>
2023-06-07 13:23:53 -0700

    added time travel vfx

---
+++ Assets/Scripts/TimeTravel.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTravel : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem timeTravelContinuous;
+    [SerializeField] private ParticleSystem timeTravelFX;
+    [SerializeField] private GameObject whiteFade;
+    [SerializeField] private Collider col;
+    public void FixedPortal()
+    {
+        timeTravelContinuous.Play();
+        col.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(ChangeEnviro());
+        }
+    }
+
+    private IEnumerator ChangeEnviro()
+    {
+        timeTravelFX.Play();
+        yield return new WaitForSeconds(1f);
+        whiteFade.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        whiteFade.SetActive(false);
+
+    }
+}
