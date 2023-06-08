d77d3437a9ff8acebb8ed92fa1937a0464f002db
Winbert Zhang <winbertzhang@gmail.com>
2023-06-08 00:10:45 -0700

    Adding Still Objects
    Boxes become still objects once puzzle is finished

--- Assets/Scripts/MainRoomPuzzle.cs
+++ Assets/Scripts/MainRoomPuzzle.cs
@@ -9,6 +9,11 @@ public class MainRoomPuzzle : MonoBehaviour
     public GameObject box2;
     public GameObject box3;
 
+    public GameObject box1Still;
+    public GameObject box2Still;
+    public GameObject box3Still;
+
+
     public XRSocketInteractor holder1;
     public XRSocketInteractor holder2;
     public XRSocketInteractor holder3;
@@ -36,7 +41,14 @@ public class MainRoomPuzzle : MonoBehaviour
     {
         Debug.Log("In checkplacement");
         if(checkBox1() && checkBox2() && checkBox3()){
-            Debug.Log("Completed");
+            //Activate portal here
+            box1.SetActive(false);
+            box2.SetActive(false);
+            box3.SetActive(false);
+
+            box1Still.SetActive(true);
+            box2Still.SetActive(true);
+            box3Still.SetActive(true);
         }
         else{
             Debug.Log("Not Enough");
